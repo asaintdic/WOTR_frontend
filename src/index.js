ee6f5cd13b38e9e501e00b4f6b5797703785cbce
Alain <alains214@gmail.com>
2020-11-12 20:35:44 -0500

    vreated store in app.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 
 import App from './App';
 
 // set up our store
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const composeEnhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+
+let store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>, 
+
+document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
